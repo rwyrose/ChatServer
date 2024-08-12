42d969f407aa11964507853718e14db991f58694
rwy <rwy980903@163.com>
2024-08-12 14:07:19 +0800

    添加自动编译脚本

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rwy/project/chat/ChatServer/src/server/chatserver.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/chatservice.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/db/db.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/main.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/model/friendmodel.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/model/groupmodel.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/model/offlinemessagemodel.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/model/usermodel.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/rwy/project/chat/ChatServer/src/server/redis/redis.cpp" "/home/rwy/project/chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
